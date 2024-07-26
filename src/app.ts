98332cf0768361d0db92a4d7d4e692ef0d730c36
The Zuraiz <thezuraiz@gmail.com>
2024-07-26 07:02:29 +0500

    add some changes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import globalErrorHander from "./middleware/globalErrorHandler";
+import { userRouter } from "./user/userRouter";
 
 const app = express();
+
+// To decode and read json data
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
@@ -8,6 +13,8 @@ app.get("/", (req, res) => {
   });
 });
 
+app.use("/api/users", userRouter);
 
+app.use(globalErrorHander);
 
 export default app;
