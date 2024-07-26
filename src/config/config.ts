18436b51f0699d97b37b540ca6810fb555810548
The Zuraiz <thezuraiz@gmail.com>
2024-07-26 07:02:53 +0500

    add firebase connection terminal

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,13 @@ const _config = {
   port: process.env.PORT,
   node_env: process.env.NODE_ENV,
   jwt_key: process.env.JWTKEY,
+  apiKey: process.env.FIREBASE_apiKey,
+  authDomain: process.env.FIREBASE_authDomain,
+  projectId: process.env.FIREBASE_projectId,
+  storageBucket: process.env.FIREBASE_storageBucket,
+  messagingSenderId: process.env.FIREBASE_messagingSenderId,
+  appId: process.env.FIREBASE_appId,
+  measurementId: process.env.FIREBASE_measurementId,
 };
 
 export const config = Object.freeze(_config);
