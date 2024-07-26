23a812c2b7f576d8eaa369ea9eafaab2d4efe0e9
The Zuraiz <thezuraiz@gmail.com>
2024-07-26 07:01:40 +0500

    add firebase connection

---
+++ src/config/db.ts
@@ -0,0 +1,31 @@
+import { config } from "./config";
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { Firestore, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+  measurementId: config.measurementId,
+};
+
+let app: FirebaseApp;
+let firestoreDB: Firestore;
+
+const initializFirebaseApp = () => {
+  try {
+    app = initializeApp(firebaseConfig);
+    firestoreDB = getFirestore(app);
+    console.log("DB Connected");
+    return app;
+  } catch (e) {
+    throw `Firestore DB connection erorr ${e}`;
+  }
+};
+
+const getFirebaseApp = () => app;
+
+export { initializFirebaseApp, getFirebaseApp, firestoreDB };
